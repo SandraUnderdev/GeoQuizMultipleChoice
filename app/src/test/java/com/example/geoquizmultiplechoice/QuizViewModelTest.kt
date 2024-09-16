b7f3881401bde76bb56f0bc021ef4e843db530cb
u412667 <sandra.opone@united.com>
2024-09-16 14:37:21 -0500

    Chapter 6 Testing

---
+++ app/src/test/java/com/example/geoquizmultiplechoice/QuizViewModelTest.kt
@@ -0,0 +1,25 @@
+package com.example.geoquizmultiplechoice
+
+import android.util.Log
+import androidx.lifecycle.SavedStateHandle
+import org.junit.Assert.*
+import org.junit.Test
+
+class QuizViewModelTest {
+    @Test
+    fun providesExpectedQuestionText() {
+        val savedStateHandle = SavedStateHandle()
+        val quizViewModel = QuizViewModel(savedStateHandle)
+        assertEquals(R.string.question_australia, quizViewModel.currentQuestionText)
+    }
+
+    @Test
+    fun wrapsAroundQuestionText() {
+        val savedStateHandle = SavedStateHandle(mapOf(CURRENT_INDEX_KEY to 5))
+        val quizViewModel = QuizViewModel(savedStateHandle)
+        assertEquals(R.string.question_asia, quizViewModel.currentQuestionText)
+        quizViewModel.moveToNext()
+        assertEquals(R.string.question_australia, quizViewModel.currentQuestionText)
+    }
+
+}
